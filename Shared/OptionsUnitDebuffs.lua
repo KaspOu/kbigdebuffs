beb6da0de94b073d4348f6be26dcbd5c71dbb46f
Ryad M <ryad@ryme-dev.com>
2024-06-03 06:37:20 +0200

    Code refactor

--- Shared/OptionsUnitDebuffs.lua
+++ Shared/OptionsUnitDebuffs.lua
@@ -10,14 +10,7 @@ local function ManageUnitDebuffsOptions()
     local hideDisabledModules = ns.FindControl("HideDisabledModules")
     local isEnabled = ns.IsModuleEnabled(activeCheckbox, headingLabel, _G[ns.OPTIONS_NAME].ActiveUnitDebuffs, hideDisabledModules and hideDisabledModules:GetChecked())
 
-    ns.OptionsEnable(ns.FindControl("BuffsScale"), isEnabled,  .2)
-    ns.OptionsEnable(ns.FindControl("MaxBuffs"), isEnabled, .2)
-    ns.OptionsEnable(ns.FindControl("BuffsPerLine"), isEnabled, .2)
-    ns.OptionsEnable(ns.FindControl("DebuffsScale"), isEnabled, .2)
-    ns.OptionsEnable(ns.FindControl("BuffsVertical"), isEnabled, .2)
-    ns.OptionsEnable(ns.FindControl("MaxDebuffs"), isEnabled, .2)
-    ns.OptionsEnable(ns.FindControl("DebuffsPerLine"), isEnabled, .2)
-    ns.OptionsEnable(ns.FindControl("DebuffsVertical"), isEnabled, .2)
+    ns.OptionsSiblingsEnable(_G[ns.OPTIONS_NAME], activeCheckbox, isEnabled, .2)
 
 end
 K_SHARED_UI.AddRefreshOptions(ManageUnitDebuffsOptions)
